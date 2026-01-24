e64ffea768ee169e0ababfda4272325cfe89dbeb
奇 <83014177+kiIIuaaa@users.noreply.github.com>
2026-01-24 16:59:33 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js - Cloudflare Workers 基础示例
-export default {
-  // 处理所有 HTTP 请求
-  async fetch(request) {
-    return new Response("Hello, Cloudflare Workers!", {
-      headers: { "Content-Type": "text/plain" },
-    });
-  },
-};
